535729b97dcb0b3a6e0d901098ae3ae449a575fd
AmanGarg07 <Amangarg7702@gmail.com>
2022-04-12 21:32:13 +0530

    Updates

---
+++ precache-manifest.2a8a7c652297a15a1acb5136e31bccef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74db24db1625d5ccaf54165647f98f43",
+    "url": "/Quiz-App/index.html"
+  },
+  {
+    "revision": "fbc1d1c60a8e53640a6a",
+    "url": "/Quiz-App/static/css/main.50566d9f.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/Quiz-App/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz-App/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc1d1c60a8e53640a6a",
+    "url": "/Quiz-App/static/js/main.ad03bf33.chunk.js"
+  },
+  {
+    "revision": "dc6ad8464665e2864c1d",
+    "url": "/Quiz-App/static/js/runtime-main.da63c0d7.js"
+  }
+]);
